0c77ea9b431be45ea37ce4ae6fe84111b443ac97
szabosandorpeter <sandor.szabo.peter@gmail.com>
2022-06-17 12:25:53 +0200

    Practise ASP

---
+++ 0518/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using _0518.Models;
+
+namespace _0518.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<_0518.Models.Aru> Aru { get; set; }
+    }
+}
---
+++ 0518/Models/Arukeres.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _0518.Models
+{
+    public class Arukeres
+    {
+        public string Megnevezes { get; set; }
+        public string Beszallito { get; set; }
+        public List<Aru> AruLista { get; set; }
+        public SelectList BeszallitoLista { get; set; }
+    }
+}
